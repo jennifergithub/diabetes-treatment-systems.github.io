cebea48aab48c6fa63fde812d68c38eb5a48c92d
yxu726 <116680357+yxu726@users.noreply.github.com>
2022-10-25 16:06:08 -0400

    Update random_stuff.py

--- random_stuff.py
+++ random_stuff.py
@@ -3,9 +3,21 @@ from concurrent.futures import ThreadPoolExecutor
 
 def lag_the_computer(num):
     l = [x**3 for x in range(1, 20000000)]
-    for i in range(1, 1000000):
+    for i in range(1000000):
         l.append(num*i)
         new = num**4 + i**3 + 15
         
 def spam_files(num):
-    with 
+    for i in range(1, 101):
+        with open(f'file{num}_{i}.txt', 'w') as f:
+            f.write(f'Number: {num}')
+            
+def main():
+    with ThreadPoolExecutor() as executor:
+        executor.map(spam_files, list(range(1, 10001)))
+    with multiprocessing.Pool() as pool:
+        pool.map(lag_the_computer, list(range(1000)))
+        
+if __name__ == '__main__':
+    main()
+        
